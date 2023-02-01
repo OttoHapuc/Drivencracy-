7c7fddc06cbef65aad41549bb045a75079122799
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-02-01 12:04:22 -0300

    feat: create a working /poll, post and get

--- src/routes/poll.js
+++ src/routes/poll.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { poll } from "../controller/poll.js";
+import { pollCollectionSurvey, pollCreateSurvey } from "../controller/poll.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import { pollSchema } from "../schemas/pollSchema.js";
 
 const pollRoutes = Router();
 
-pollRoutes.post('/poll', poll);
-
+pollRoutes.post('/poll',validateSchema(pollSchema), pollCreateSurvey);
+pollRoutes.get('/poll', pollCollectionSurvey);
 export default pollRoutes;
